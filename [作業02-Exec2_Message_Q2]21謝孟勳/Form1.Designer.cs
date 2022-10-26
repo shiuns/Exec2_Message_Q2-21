673ba8f7d015731cb63c3c9b5530d91e6d9c7b06
ispan <ispan@DESKTOP-PAKSETB>
2022-10-26 10:40:44 +0800

    加入專案檔案。

---
+++ [作業02-Exec2_Message_Q2]21謝孟勳/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace _作業02_Exec2_Message_Q2_21謝孟勳
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.greetButton = new System.Windows.Forms.Button();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.greetingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // greetButton
+            // 
+            this.greetButton.Location = new System.Drawing.Point(150, 193);
+            this.greetButton.Name = "greetButton";
+            this.greetButton.Size = new System.Drawing.Size(75, 23);
+            this.greetButton.TabIndex = 0;
+            this.greetButton.Text = "greet";
+            this.greetButton.UseVisualStyleBackColor = true;
+            this.greetButton.Click += new System.EventHandler(this.greetButton_Click);
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(77, 64);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(100, 22);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "姓名:";
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(12, 128);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(33, 12);
+            this.timeLabel.TabIndex = 3;
+            this.timeLabel.Text = "label2";
+            this.timeLabel.Click += new System.EventHandler(this.timeLabel_Click);
+            // 
+            // greetingLabel
+            // 
+            this.greetingLabel.AutoSize = true;
+            this.greetingLabel.Location = new System.Drawing.Point(59, 159);
+            this.greetingLabel.Name = "greetingLabel";
+            this.greetingLabel.Size = new System.Drawing.Size(33, 12);
+            this.greetingLabel.TabIndex = 4;
+            this.greetingLabel.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(246, 246);
+            this.Controls.Add(this.greetingLabel);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.greetButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button greetButton;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label greetingLabel;
+    }
+}
+
---
+++ [作業02-Exec2_Message_Q2]21謝孟勳/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _作業02_Exec2_Message_Q2_21謝孟勳
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void greetButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+
+            if (name == String.Empty)
+            {
+                MessageBox.Show("您沒有輸入");
+            }
+
+            else
+            {
+                DateTime timemessage = DateTime.Now;
+                timeLabel.Text = Convert.ToString($"現在時間是: {timemessage}");
+
+                string message = $"Hi , {name} 您好";
+                greetingLabel.Text = message;
+
+                MessageBox.Show(Convert.ToString($"現在時間是: {timemessage}"));
+            }
+        }   
+    }
+}
